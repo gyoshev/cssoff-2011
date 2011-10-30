398131069683bdf5c8e0401c5de30ab100e342c0
Alex Gyoshev <alex@gyoshev.net>
2011-10-30 19:16:48 +0200

    Finished applying design

--- triple-dare.js
+++ triple-dare.js
@@ -1,6 +1,12 @@
 (function($) {
     $.closest = function(element, selector) {
-        return $dom.is(element, selector) ? element : $dom.ancestor(element,selector);
+        return $.is(element, selector) ? element : $.ancestor(element,selector);
+    };
+
+    $.each = function(elements, handler) {
+        for (var i = 0; i < elements.length; i++) {
+            handler.call(elements[i], i);
+        }
     };
 
     $.siblings = function(element) {
@@ -30,58 +36,88 @@
         }
     };
 
-    $.onready(function() {
-        // obstacles
-        var obstacles = $.get("#obstacles a");
-
-        $.onclick(obstacles, function(e) {
-            e.preventDefault();
-
-            var descendants = $.descendants,
-                container = $.closest(e.target, "a"),
-                img = $.descendants(container, "img");
-
-            // change container styles
-            var siblings = $.siblings(container.parentNode)
-            for (var i = 0; i < siblings.length; i++) {
-                $.removeClass(siblings[i], "selected");
-            }
-
-            $.addClass(container.parentNode, "selected");
-
-            var info = $.get("#obstacles div.column")[0];
-
-            // change image
-            descendants(info, "img")[0].src = img[0].src.replace(/\.png$/i, "_460.png");
-
-            // change text
-            descendants(info, "h3")[0].innerHTML = img[0].alt;
-            descendants(info, "h4")[0].innerHTML = descendants(container, ".description")[0].innerHTML;
-
-            return false;
-        });
-
-        // clock
-        var clock = $.get("#clock")[0];
-
-        function pad(i) {
-            return i < 10 ? "0" + i : i;
+    $.getStyle = function getStyle(element, property){
+        if (element.currentStyle) {
+            return element.currentStyle[property];
+        } else if (document.defaultView && document.defaultView.getComputedStyle) {
+            return document.defaultView.getComputedStyle(element, "")[property];
         }
+    }
 
-        var secondsInterval = setInterval(function() {
-            var secondsNode = clock.firstChild,
-                secondsRemaining = +secondsNode.nodeValue - 1;
 
-            if (secondsRemaining == 0) {
-                clearInterval(secondsInterval);
+    var TripleDare = {
+        initObstacles: function() {
+            // obstacles
+            var obstacles = $.get("#obstacles a");
 
-                // enable blinking
-                setInterval(function() {
-                    secondsNode.nodeValue = secondsNode.nodeValue == "00" ? "" : "00";
-                }, 500);
+            $.onclick(obstacles, function(e) {
+                e.preventDefault();
+
+                var descendants = $.descendants,
+                    container = $.closest(e.target, "a"),
+                    img = $.descendants(container, "img");
+
+                // change container styles
+                $.each($.siblings(container.parentNode), function() {
+                    $.removeClass(this, "selected");
+                });
+
+                $.addClass(container.parentNode, "selected");
+
+                var info = $.get("#obstacles div.column")[0];
+
+                // change image
+                descendants(info, "img")[0].src = img[0].src.replace(/\.png$/i, "_460.png");
+
+                // change text
+                descendants(info, "h3")[0].innerHTML = img[0].alt;
+                descendants(info, "h4")[0].innerHTML = descendants(container, ".description")[0].innerHTML;
+
+                return false;
+            });
+        },
+        initClock: function() {
+            var clock = $.get("#clock")[0];
+
+            function pad(i) {
+                return i < 10 ? "0" + i : i;
             }
 
-            secondsNode.nodeValue = pad(secondsRemaining);
-        }, 1000);
+            var secondsInterval = setInterval(function() {
+                var secondsNode = clock.firstChild,
+                    secondsRemaining = +secondsNode.nodeValue - 1;
+
+                if (secondsRemaining == 0) {
+                    clearInterval(secondsInterval);
+
+                    // enable blinking
+                    setInterval(function() {
+                        secondsNode.nodeValue = secondsNode.nodeValue == "00" ? "" : "00";
+                    }, 500);
+                }
+
+                secondsNode.nodeValue = pad(secondsRemaining);
+            }, 1000);
+        },
+        initSelectBoxes: function() {
+            $.each($.get("select"), function() {
+                var wrap = $.create("div.select");
+                this.parentNode.insertBefore(wrap, this);
+
+                wrap.style.width = this.offsetWidth + "px";
+                wrap.style.height = this.offsetHeight + "px";
+                wrap.style.marginRight = $.getStyle(this, "marginRight");
+
+                wrap.appendChild(this);
+                wrap.appendChild(document.createTextNode(this.value));
+                wrap.appendChild($.create("span.arrow"));
+            });
+        }
+    };
+
+    $.onready(function() {
+        TripleDare.initObstacles();
+        TripleDare.initClock();
+        TripleDare.initSelectBoxes();
     });
 })($dom);
